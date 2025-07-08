c7dfaae4fb31485a731b06b8ff8b62f0e2d12655
Carto1a <49589600+Carto1a@users.noreply.github.com>
2025-07-08 15:41:49 -0300

    refactor: better names

--- ase.go
+++ ase.go
@@ -79,7 +79,7 @@ type ChunkColorProfileData struct {
 	_     [8]byte
 }
 
-func magicNumber(magic, number uint16, from ...string) {
+func checkMagicNumber(magic, number uint16, from ...string) {
 	fmt.Println(from, "magic number: ", magic, number)
 	if number != magic {
 		log.Fatalf(strings.Join(from, ""), "magic number fail")
@@ -87,7 +87,7 @@ func magicNumber(magic, number uint16, from ...string) {
 	fmt.Println(from, "magic number pass")
 }
 
-func readChunk(reader io.Reader, buffer *bytes.Buffer, chunk []byte) error {
+func readToBuffer(reader io.Reader, buffer *bytes.Buffer, chunk []byte) error {
 	_, err := reader.Read(chunk)
 	if err != nil {
 		return err
@@ -101,16 +101,16 @@ func readChunk(reader io.Reader, buffer *bytes.Buffer, chunk []byte) error {
 	return nil
 }
 
-func temespacope(size int, buffer *bytes.Buffer) bool {
+func enoughSpaceToRead(size int, buffer *bytes.Buffer) bool {
 	available := buffer.Len()
 	needed := size
 	fmt.Println("available: ", available, "needed: ", needed)
 	return available >= needed
 }
 
-func negocioAntesDeLerELerTbmERetornarOObj[T any](size int, buffer *bytes.Buffer) T {
+func BytesToStruct[T any](size int, buffer *bytes.Buffer) T {
 	var t T
-	if temespacope(size, buffer) {
+	if enoughSpaceToRead(size, buffer) {
 		err := binary.Read(buffer, binary.LittleEndian, &t)
 		if err != nil {
 			fmt.Println("binary.Read: ", err)
@@ -119,21 +119,21 @@ func negocioAntesDeLerELerTbmERetornarOObj[T any](size int, buffer *bytes.Buffer
 	}
 
 	fmt.Println("pegar mais dados do fd")
-	readChunk(Reader, buffer, ChunkPart)
-	return negocioAntesDeLerELerTbmERetornarOObj[T](size, buffer)
+	readToBuffer(Reader, buffer, ChunkPart)
+	return BytesToStruct[T](size, buffer)
 }
 
-func cuidardochunk(ch ChunkHeader, buffer *bytes.Buffer) []byte {
+func loadFrameChunkData(ch ChunkHeader, buffer *bytes.Buffer) []byte {
 	size := ch.Size - ChunkHeaderSize
-	if temespacope(int(size), buffer) {
+	if enoughSpaceToRead(int(size), buffer) {
 		bufchunk := make([]byte, size)
 		io.ReadFull(buffer, bufchunk)
 		return bufchunk
 	}
 
 	fmt.Println("chunk: pegar mais dados do fd")
-	readChunk(Reader, buffer, ChunkPart)
-	return cuidardochunk(ch, buffer)
+	readToBuffer(Reader, buffer, ChunkPart)
+	return loadFrameChunkData(ch, buffer)
 }
 
 func main() {
@@ -147,7 +147,7 @@ func main() {
 	buffer := new(bytes.Buffer)
 	fmt.Println("buffer available on create: ", buffer.Len())
 	Reader = fd
-	readChunk(Reader, buffer, ChunkPart)
+	readToBuffer(Reader, buffer, ChunkPart)
 	fmt.Println("buffer available after a read fd: ", buffer.Len())
 
 	var h Header
@@ -155,30 +155,30 @@ func main() {
 	binary.Read(buffer, binary.LittleEndian, &h)
 	fmt.Println("buffer available after a read to struct: ", buffer.Len())
 
-	magicNumber(0xA5E0, h.MagicNumber, "header")
+	checkMagicNumber(0xA5E0, h.MagicNumber, "header")
 
 	buffer.Reset()
 
 	for i := range h.Frames {
-		readChunk(Reader, buffer, ChunkPart)
+		readToBuffer(Reader, buffer, ChunkPart)
 
 		var fh FrameHeader
 		fmt.Println("framaeheader to struct")
-		fh = negocioAntesDeLerELerTbmERetornarOObj[FrameHeader](FrameHeaderSize, buffer)
-		magicNumber(0xF1FA, fh.MagicNumber, "frameheader", fmt.Sprint(i))
+		fh = BytesToStruct[FrameHeader](FrameHeaderSize, buffer)
+		checkMagicNumber(0xF1FA, fh.MagicNumber, "frameheader", fmt.Sprint(i))
 
 		fmt.Println("Chunk number: ", fh.ChunkNumber)
 
 		for range fh.ChunkNumber {
 			var ch ChunkHeader
 			fmt.Println("chunkheader to struct")
-			ch = negocioAntesDeLerELerTbmERetornarOObj[ChunkHeader](ChunkHeaderSize, buffer)
+			ch = BytesToStruct[ChunkHeader](ChunkHeaderSize, buffer)
 
 			fmt.Println("Chunk type: ", ch.Type)
 			fmt.Println("Chunk size: ", ch.Size)
 
 			fmt.Println("chunkdata to struct")
-			bufchunk := cuidardochunk(ch, buffer)
+			bufchunk := loadFrameChunkData(ch, buffer)
 
 			c := &Chunk{
 				Header: ch,
